0f29923ae5a8274be83cbec943c445582b65b55c
Sanusi Lamido <81477186+SanusiLamido@users.noreply.github.com>
2023-12-03 15:13:01 +0100

    3rd commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import img from "../assets/img/about.jpg";
+import img from "../assets/img/book.jpg";
+import {HiCheckBadge} from "react-icons/hi2";
 
 const About = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col lg:flex-row items-center justify-between lg:px-32 px-5">
       <div>
-        {/* <img src={img} alt="img" /> */}
+        {/* { <img src={img} alt="img" /> } */}
       </div>
       <div>
         <h1>About Products</h1>
         <h2>Experience Book shopping redefined</h2>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+        <div>
+            <HiCheckBadge size={25} className="text-backgroundColor"/>
+        </div>
+        <h2> Fiction </h2>
       </div>
     </div>
   );
